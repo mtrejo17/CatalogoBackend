11e9e475cf8822e8b6f9a409f11445449ecc853a
marcosIngenya <marcos@ingenya.com.mx>
2020-07-01 07:48:41 -0500

    Avance de usuarioController

--- lib/config.ts
+++ lib/config.ts
@@ -1,2 +1,3 @@
 export const SERVER_PORT: number = 4000;
 export const urlDB: string = 'mongodb://localhost:27017/catalogo';
+export const PASSWORD_KEY: string = 'mIcLaVe';
---
+++ lib/usuario/controllers/usuarioController.ts
@@ -0,0 +1,94 @@
+import {Request, Response } from 'express';
+import Usuario from 'usuario/models/usuarioModel';
+import * as bcrypt from 'bcryptjs';
+
+export class UsuarioController {
+    public crearUsuario = (req: Request, res: Response) => {
+        let usuario = new Usuario(
+            {
+                apellidoPaterno: req.body.apellidoPaterno,
+                apellidoMaterno: req.body.apellidoMaterno,
+                nombre: req.body.nombre,
+                login: req.body.login,
+                password: bcrypt.hashSync(req.body.password,10)
+            }
+        );
+        usuario.save((err, usuarioCreado) => {
+            if (err) {
+                res.status(400).json(
+                    {
+                        ok: false,
+                        message: 'Usuario no creado',
+                        error: err
+                    }
+                );
+            }
+            res.status(201).json(
+                {
+                    ok: true,
+                    message: 'Usuario creado',
+                    usuario: usuarioCreado
+                }
+            )
+        });
+    }
+
+    public obtenerUsuarios = (req: Request, res: Response) => {
+        Usuario.find({})
+        .select('apellidoPaterno apellidoMaternonombre')
+        .exec()
+        .then(usuarios => {
+            res.status(200).json({
+                ok: true,
+                usuarios
+            })
+        })
+        .catch(error => {
+            res.status(400).json(
+                {
+                    ok: false,
+                    error
+                }
+            )
+        });
+    }
+
+    public ingresar = (req: Request, res: Response) => {       
+        Usuario.findOne({login: req.body.login})
+        .exec()
+        .then(usuario => {
+            if (!usuario) {
+                return res.status(401).json(
+                    {
+                        ok: false,
+                        message: 'Usuario no autorizado'
+                    }
+                )
+            } else {
+                if (bcrypt.compareSync(req.body.password, usuario.password)) {
+                    res.status(200).json({
+                        ok: true,
+                        message: 'Usuario autorizado'
+                    });
+                } else {
+                    return res.status(401).json(
+                        {
+                            ok: false,
+                            message: 'Usuario no autorizado'
+                        }
+                    );
+                }
+            }
+        })
+        .catch(error => {
+            return res.status(401).json(
+                {
+                    ok: false,
+                    message: 'Usuario no autorizado'
+                }
+            );
+        });
+
+    }
+
+}
